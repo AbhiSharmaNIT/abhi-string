48190df4897f5679f8673fa6a5af6457ae6843b1
Abhishek Kumar <136017324+AbhiSharmaNIT@users.noreply.github.com>
2023-10-16 14:54:42 +0530

    Add files via upload

---
+++ string1.cpp
@@ -0,0 +1,33 @@
+//// How to declear a string in deferent type of formates ?
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+  // 1.   string st;
+  //  cout<<"Enter a string : ";
+  //  cin>>st;
+  //  cout<<st;
+
+  // 2.  string st;
+  //     string st1(5,'P');
+  //    // cin>>st1;     cin is not required because we have already take the values in the string 1 .
+  //     cout<<st1;
+
+  //  string st = "I love you .";
+  //     cout<<st;
+
+  //  // string st;
+  //   getline(cin,st);      // getline is used for print all string whose we have to intputed in the program.
+  //   cout<<st;             //  if we will use cin then we have not got all strings, we get only first string.
+  ////Addition of two string using append function
+//   string st1 = "man";
+//   string st2 = "woman";
+//   st1.append(st2); // append is used to add two strings.
+//   //                   string st3 = st1 + st2;    //Without using append we will be using this type of algorithm
+//   cout << st1 << endl;
+
+//   return 0;
+// }
